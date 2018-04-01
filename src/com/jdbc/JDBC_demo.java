aa51b65a5a989059f0df09f6d6c11856846bf3c1
FaizaHuma <fhuma46@gmail.com>
2018-04-02 02:34:47 +0600

    jdbc_commit

---
+++ src/com/jdbc/JDBC_demo.java
@@ -0,0 +1,51 @@
+package com.jdbc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+public class JDBC_demo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out=response.getWriter();
+
+        out.println("<h1>hello JDBC_DEMO</h1>");
+
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection con=DriverManager.getConnection(" jdbc:oracle:thin:@localhost:1521:orcl ","Computer_lab","computer_lab");
+            Statement stmt=con.createStatement();
+            ResultSet rst=stmt.executeQuery("select*from computer_student");
+
+            while(rst.next())
+
+            {    String computer_student=rst.getString(2);
+
+                int student_id=rst.getInt(1);
+                int student_age=rst.getInt(3);
+
+                out.println("<h1>"+computer_student+" "+student_id+" "+student_age+"</h1>");
+            }
+            con.close();
+
+
+        }
+        catch(Exception e)
+        {
+            out.println("<h3>"+e+"</h3>");
+        }
+
+
+
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 4/1/2018
+  Time: 7:55 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+
+  <h1>click here<a href="hello">go</a> </h1>
+
+  $END$
+  </body>
+</html>
